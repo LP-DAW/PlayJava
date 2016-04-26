4ee60c4530116f4a315c84ff7710f07705fe4d6e
Profesor.asix <profesor.asix@gmail.com>
2016-04-26 10:44:49 +0200

    Student branch

--- src/main/java/es/ceed/App.java
+++ src/main/java/es/ceed/App.java
@@ -31,28 +31,25 @@ public class App
         //PASO 1 . Creamos y añadimos los ordenadores a un ArrayList denominado ordenadores
         System.out.print("\nPASO1 Añadimos los ordenadores a la lista\n" );
         System.out.print("===========================================\n\n" );
-        ArrayList<Ordenador> ordenadores = new ArrayList<Ordenador>(Arrays.asList(ord1,ord2,ord3,ord4));
+
 
         // PASO 2. Lee/Recupera el ordenador que ocupe la posición 2 en la lista, Muestralo por consola (print)
         System.out.print("\nPASO2 Visualiza ordenador posción 2\n" );
         System.out.print("===================================\n\n" );
-        System.out.println(ordenadores.get(2));
+
 
 
         // PASO 3. Elimina el ordenador que ocupe la posición 3 en la lista
         System.out.print("\nPASO3 Eliminamos el elemento que ocupa la posición 3\n" );
         System.out.print("======================================================\n\n" );
-        ordenadores.remove(3);
+
 
         // PASO 4. Muestra por consola, utilzando print y un for todos los elementos de la lista,
         // no se debería mostrar el elemento 3
 
         System.out.print("\nPASO4 Muestra todos los ordenadores de la lista 2\n" );
         System.out.print("=================================================\n\n" );
-        for (Ordenador ord : ordenadores)
-        {
-            System.out.println(ord);
-        }
+
 
         // PASO 5. Protege la lista de duplicados, es decir, no deberían existir dos ordenadores con la misma referencia
         // en la lista, para ello será necesario implementar el método equals en Ordenador (ya hecho) y utilizar de forma
@@ -60,11 +57,6 @@ public class App
         System.out.print("\nPASO5 Comprueba que el ordenador con la misma referencia ya existe en la lista\n" );
         System.out.print("==============================================================================\n" );
 
-        if (ordenadores.contains(new Ordenador("ORD0002"))) {
-            System.out.println("ORD0002 Ya existe");
-        } else {
-            System.out.println("ORD0002 Introducido");
-        }
 
     }
 }
