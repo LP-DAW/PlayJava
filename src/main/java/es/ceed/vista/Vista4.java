3598dec95cc37684658749b558bbdff372297fc7
Profesor.asix <profesor.asix@gmail.com>
2016-05-02 20:14:34 +0200

    Vista4 updated

--- src/main/java/es/ceed/vista/Vista4.java
+++ src/main/java/es/ceed/vista/Vista4.java
@@ -8,20 +8,39 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Vista4 extends JFrame {
+
+    JTextField op1;
+    JTextField op2;
+    JTextField res;
+    JButton suma;
+
     public Vista4() {
         super("ex31gui");
         init();
     }
 
     private void init() {
-       
+
+        // 1. inicializa componentes
+
+        suma.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                sumaActionPerformed(evt);
+            }
+        });
+
         setResizable(true);
         setLocationRelativeTo(null); //centrar en pantalla
         pack();
     }
 
+    private void sumaActionPerformed(java.awt.event.ActionEvent evt) {
+        ObtenerSuma();
+    }
 
+    public void ObtenerSuma() throws NumberFormatException {
+        // 2. Caluclar la suma y actualizar el JTextField res
 
+    }
 }
-
 
